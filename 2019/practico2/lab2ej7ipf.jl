d72f1dd2abc96cf35e3636eaed30f578fbf7cc40
diegolupi93 <diegolupi93@gmail.com>
2019-04-25 19:29:32 -0300

    punto fijo

--- 2019/practico2/lab2ej7ipf.jl
+++ 2019/practico2/lab2ej7ipf.jl
@@ -2,7 +2,7 @@ include("ripf.jl")
 
 
 function fun(y, a=x)
-    return y - exp(-((1-a*y)^2)) 
+    return exp(-((1-a*y)^2)) 
 end
 
 function lab2ej7newton(fun,x0,err,iter)
@@ -11,8 +11,8 @@ function lab2ej7newton(fun,x0,err,iter)
     for i in 0.0:0.1:1.5 # i itera de 0 a 1.5 en intervalos de 0.1
         global x = i # uso global para fijarle el x en fun(y, a=x)
         result = ripf(fun,x0,10^-5,100)
-        last_x = result[1][length(result[1])] #es el ultimo x que obtengo en el metodo de newton
-        eval = fun(last_x)  # evaluo la fun en el ultimo x que obtengo en metodo de newton, esto quiere decir el mas cercano a la raiz
+        last_x = result[1][length(result[1])] #es el ultimo x que obtengo en el metodo de punto fijo
+        eval = fun(last_x)  # evaluo la fun en el ultimo x que obtengo en metodo de punto fijo, esto quiere decir el mas cercano a la raiz
         if abs(eval) < err # si es menor al error, encontre la y que se aproxima a la raiz
             append!(hx,last_x)
             append!(hf,eval)
@@ -21,6 +21,6 @@ function lab2ej7newton(fun,x0,err,iter)
     return [hx,hf] 
 end
 
-res = ripf(fun_lab2ej6,0.7,10^-5,100)
+res = ripf(fun,0.7,10^-5,100)
 using Plots
 plot(res[1],res[2],title="lab2ej7puntofijo",label=["y - exp(-((1-a*y)^2))" ""],lw=3)
