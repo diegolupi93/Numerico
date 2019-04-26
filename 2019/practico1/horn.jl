f2afaec035346ddabdf18cd888978bfc13da66dd
diegolupi <diegolupi93@gmail.com>
2019-04-26 13:56:43 -0300

    horn

--- 2019/practico1/horn.jl
+++ 2019/practico1/horn.jl
@@ -1,13 +1,13 @@
 function horn(coefs,x)
     n = length(coefs)
-    b = coefs[n]
+    b = coefs[1]
 
-    for i = n-1:-1:1
+    for i = 2:n
         b = coefs[i] + b*x
     end
     return b 
 end
 
 
-a = horn([1 -5 6 2],2)
-println(a)
+#a = horn([ 1 -5 6 2 2],2)
+#println(a)
