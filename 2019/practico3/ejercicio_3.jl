006e1e4b19255f11e743dbbfbdf1e3874d62f6b2
diegolupi <diegolupi93@gmail.com>
2019-05-07 11:30:08 -0300

    ej_3 practico 3

---
+++ 2019/practico3/ejercicio_3.jl
@@ -0,0 +1,23 @@
+include("inewton.jl")
+
+function fun(x)
+    return 1/x
+end
+
+function eje_3(fun)
+    z = []
+    x = []
+    y = []
+    C = 24/25 #LinRange(1,5,101)
+    for i in 1:101
+        append!(z, C+j/25)
+    end
+    for i in 1:5
+        append!(x,i)
+        append!(y,fun(i))
+    end
+    return inewton(x,y,z), y, z
+end
+
+pol, f, z = eje_3(fun)
+
